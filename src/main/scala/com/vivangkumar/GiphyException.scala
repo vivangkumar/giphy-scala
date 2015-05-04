5e555557076edb4b459db9724cf3ef0b412ccc16
Vivan <vivangkumar@gmail.com>
2015-05-04 11:02:48 +0100

    Comments

--- src/main/scala/com/vivangkumar/GiphyException.scala
+++ src/main/scala/com/vivangkumar/GiphyException.scala
@@ -1,9 +1,25 @@
 package com.vivangkumar
 
+/**
+ * Base class that inherits from Exception
+ * @param message Exception message
+ */
 abstract class GiphyException(message: String) extends Exception
 
+/**
+ * Exception to be thrown when a Request error occurs
+ * @param message Exception message
+ */
 class RequestException(message: String) extends GiphyException(message)
 
+/**
+ * Validation exceptions
+ * @param message Exception message
+ */
 class ValidationException(message: String) extends GiphyException(message)
 
+/**
+ * Convenience invalid validation rule exception
+ * @param message Exception message
+ */
 class InvalidValidationRuleException(message: String) extends GiphyException(message)
--- src/main/scala/com/vivangkumar/Types.scala
+++ src/main/scala/com/vivangkumar/Types.scala
@@ -1,9 +1,13 @@
 package com.vivangkumar
 
-
+/**
+ * Types for responses and validation
+ */
 object Types {
 
   type GiphyResponse = Either[GiphyException, Map[String, Any]]
 
   type ValidationResponse = Either[GiphyException, Boolean]
+
 }
+
